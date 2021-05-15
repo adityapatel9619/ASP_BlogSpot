59c38257c3c17e861931d79b072b9955d3dfa646
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2021-05-15 14:02:21 +0530

    update

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_BlogSpot.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult WhatsTrending()
+        {
+            return View();
+        }
+    }
+}
