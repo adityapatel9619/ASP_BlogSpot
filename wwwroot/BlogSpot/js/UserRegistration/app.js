5baffebd54d77c901ab1eecbde2b80c24acf7385
adityapatel9619 <adityapatel9619@gmail.com>
2021-07-13 20:12:43 +0530

    Aditya CM(UserRegistration)

---
+++ wwwroot/BlogSpot/js/UserRegistration/app.js
@@ -0,0 +1,31 @@
+﻿var mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+    scrollFunction();
+}
+
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+
+document.getElementById('myBtn').addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+window.onload = function () {
+    document.getElementById("loading").style.display = 'block';
+    setTimeout(showData, 1000);
+}
+
+function showData() {
+    document.getElementById("loading").style.display = 'none';
+    document.getElementById("content-body").style.display = 'block';
+}
