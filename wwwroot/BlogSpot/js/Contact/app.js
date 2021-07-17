51675d32162cbbaa34c9c517e3b6b96020758226
adityapatel9619 <adityapatel9619@gmail.com>
2021-07-17 19:20:16 +0530

    Aditya CM(Contact)

--- wwwroot/BlogSpot/js/Contact/app.js
+++ wwwroot/BlogSpot/js/Contact/app.js
@@ -1,1 +1,30 @@
-﻿
+﻿document.getElementById('mynewBtn').addEventListener('click', function (e) {
+
+    validateForm();
+
+    e.preventDefault();
+})
+
+function validateForm() {
+    let name = document.getElementById("txtName").value;
+    let email = document.getElementById("txtEmail").value;
+
+    if (name == "" || name == null) {
+        document.getElementById("txtName").classList.add("is-invalid");
+    }
+    else {
+        document.getElementById("txtName").classList.remove("is-invalid");
+        document.getElementById("txtName").classList.add("is-valid");
+    }
+
+    if (email == "" || email == null) {
+        document.getElementById("txtEmail").classList.add("is-invalid");
+    }
+    else {
+        document.getElementById("txtEmail").classList.remove("is-invalid");
+        document.getElementById("txtEmail").classList.add("is-valid");
+    }
+
+}
+
+
--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -21,3 +21,4 @@ document.getElementById('myBtn').addEventListener('click', function () {
 })
 
 
+
